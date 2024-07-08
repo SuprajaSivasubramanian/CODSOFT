0446c81aad3e2f7fdd3391b98ee2b9b15fe97e9e
SuprajaSivasubramanian <139612001+SuprajaSivasubramanian@users.noreply.github.com>
2024-07-08 13:28:58 +0530

    Add files via upload

---
+++ calculator/script.js
@@ -0,0 +1,61 @@
+let display = document.getElementById('display');
+let currentInput = '';
+let previousInput = '';
+let operation = undefined;
+
+function appendNumber(number) {
+    if (currentInput === '0' && number === '0') return;
+    if (currentInput === '0') currentInput = '';
+    currentInput += number;
+    updateDisplay();
+}
+
+function chooseOperation(op) {
+    if (currentInput === '') return;
+    if (previousInput !== '') {
+        compute();
+    }
+    operation = op;
+    previousInput = currentInput;
+    currentInput = '';
+}
+
+function compute() {
+    let computation;
+    const prev = parseFloat(previousInput);
+    const current = parseFloat(currentInput);
+    if (isNaN(prev) || isNaN(current)) return;
+    switch (operation) {
+        case '+':
+            computation = prev + current;
+            break;
+        case '-':
+            computation = prev - current;
+            break;
+        case '*':
+            computation = prev * current;
+            break;
+        case '/':
+            computation = prev / current;
+            break;
+        default:
+            return;
+    }
+    currentInput = computation;
+    operation = undefined;
+    previousInput = '';
+    updateDisplay();
+}
+
+function clearDisplay() {
+    currentInput = '';
+    previousInput = '';
+    operation = undefined;
+    updateDisplay();
+}
+
+function updateDisplay() {
+    display.innerText = currentInput;
+}
+
+clearDisplay();
